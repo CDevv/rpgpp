326641cf2b5849c88f206d8b51da96af823aa1ab
CDevv <cdevyt@gmail.com>
2025-09-11 09:49:26 +0300

    Experimental docs with Doxide

--- include/atlasTile.hpp
+++ include/atlasTile.hpp
@@ -3,13 +3,28 @@
 
 #include <raylib.h>
 
+/**
+ * Represents a source atlas tile from a TileSet.
+ */
 class AtlasTile {
 private:
     Texture *texture;
     Vector2 atlasCoords;
 public:
+    /**
+     * Empty constructor
+     */
     AtlasTile();
+
+    /**
+     * Constructor that takes in a texture and coordinates on the atlas (the tileset)
+     */
     AtlasTile(Texture *texture, Vector2 atlasCoords);
+
+    /**
+     * Get the atlas coordinates
+     * @returns A Vector2 which represents the atlas coordinates
+     */
     Vector2 getAtlasCoords();
 };
 
--- include/tile.hpp
+++ include/tile.hpp
@@ -4,6 +4,9 @@
 #include "atlasTile.hpp"
 #include <raylib.h>
 
+/**
+ * A tile that may be placed in the world.
+ */
 class Tile {
 private:
     AtlasTile atlasTile;
@@ -11,10 +14,34 @@ private:
     bool placed;
 public:
     Tile();
+
+    /**
+     * Check if it was placed or not.
+     */
     bool isPlaced();
+
+    /**
+     * Place the tile using a source AtlasTile and world coordinates.
+     * @param atlasTile The source atlas tile from a TileSet.
+     * @param worldCoords A Vector2, representing the coordinates on the world where it will be placed.
+     */
     void place(AtlasTile atlasTile, Vector2 worldCoords);
+
+    /**
+     * Erase the tile, no longer to be shown in-game.
+     */
     void erase();
+
+    /**
+     * Get the source AtlasTile that originates from the used TileSet.
+     * @returns The source AtlasTile.
+     */
     AtlasTile getAtlasTile();
+
+    /**
+     * Get the world coordinates, at which the Tile is placed.
+     * @returns The world coordinates.
+     */
     Vector2 getWorldCoords();
 };
 
