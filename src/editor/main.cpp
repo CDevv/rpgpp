15fb9331c9b958748c9a6c6ee78f3c29ec1d7605
CDevv <cdevyt@gmail.com>
2025-09-20 11:34:59 +0300

    Basic Dear ImGui Demo
    -Add needed include files for rlImGui
    -Show Dear ImGui demo window

--- src/editor/main.cpp
+++ src/editor/main.cpp
@@ -1,33 +1,42 @@
 #include <raylib.h>
 #define RAYGUI_IMPLEMENTATION
 #include <raygui.h>
+#include <imgui.h>
+#include <rlImGui.h>
 #include "editor.hpp"
 
 int main()
 {
     const int width = 1280;
-    const int height = 640;
+    const int height = 720;
 
-    InitWindow(width, height, "Editor");
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow(width, height, "Editor [Dear ImGui]");
 
-    Editor editor;
-    editor.init();
+    //Editor editor;
+    //editor.init();
 
     SetTargetFPS(60);
 
+    rlImGuiSetup(true);
+
     while (!WindowShouldClose()) {
-        editor.update();
+        //editor.update();
 
         BeginDrawing();
+        rlImGuiBegin();
 
         ClearBackground(RAYWHITE);
 
-        editor.draw();
+        //editor.draw();
+        ImGui::ShowDemoWindow();
 
+        rlImGuiEnd();
         EndDrawing();
     }
 
-    editor.unload();
+    //editor.unload();
+    rlImGuiShutdown();
     CloseWindow();
 
     return 0;
