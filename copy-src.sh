e9c29a6d95e0e2cb8b2d2300bc1ba876a86e4917
Kaloyan Ivanov <cdevyt@gmail.com>
2026-01-10 13:17:35 +0200

    Remove unused files

--- copy-src.sh
+++
@@ -1,2 +0,0 @@
-cp build/linux/x86_64/debug/librpgpplua.so game-src/lib/librpgpplua.so
-cp build/linux/x86_64/debug/librpgpp.a game-src/lib/librpgpp.a
--- deps.sh
+++
@@ -1,5 +0,0 @@
-git submodule update --init --recursive
-
-make -C ./libs/luajit/
-cp ./libs/luajit/src/luajit ./execs/luajit
-cp ./libs/luajit/src/libluajit.so ./libs/libluajit.so
