929e7ba2cac81616e444256496b67f70467f758b
D. Quan <60545346+sudoker0@users.noreply.github.com>
2026-02-28 13:32:34 +0700

    Added translations for empty view

--- src/editor/fileViews/emptyView.cpp
+++ src/editor/fileViews/emptyView.cpp
@@ -1,11 +1,14 @@
 #include "fileViews/emptyView.hpp"
 #include "TGUI/Widgets/Group.hpp"
 #include "TGUI/Widgets/Label.hpp"
+#include "editor.hpp"
+#include "services/translationService.hpp"
 #include "variant.hpp"
 
 EmptyFileView::EmptyFileView() {
+	TranslationService &ts = Editor::instance->getTranslations();
 	auto noticeText =
-		tgui::Label::create("No file opened or view not defined yet...");
+		tgui::Label::create(ts.getKey("screen.empty.notice"));
 	noticeText->setPosition("50%", "50%");
 	noticeText->setOrigin(0.5, 0.5);
 	noticeText->setTextSize(24);
