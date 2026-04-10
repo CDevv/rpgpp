0e950fff46652e3ca45d0e62533311351dfbf37c
D. Quan <60545346+sudoker0@users.noreply.github.com>
2026-04-10 10:23:32 +0700

    Small touchup on the project settings dialog

--- src/editor/childWindows/projectSettingsWindow.cpp
+++ src/editor/childWindows/projectSettingsWindow.cpp
@@ -5,22 +5,22 @@
 #include "TGUI/Widgets/GrowVerticalLayout.hpp"
 #include "TGUI/Widgets/Label.hpp"
 #include "childWindows/popupWindow.hpp"
+#include "childWindows/settingsPanel/base.hpp"
 #include "editor.hpp"
 #include "project.hpp"
 #include "widgets/propertyFields/textField.hpp"
 
 ProjectSettingsWindow::ProjectSettingsWindow() : PopupWindow("ProjectSettings") {
 	auto layout = tgui::GrowVerticalLayout::create();
+	layout->getRenderer()->setPadding(4);
 	layout->getRenderer()->setSpaceBetweenWidgets(4.0f);
 
-	auto titleLabel = tgui::Label::create();
-	titleLabel->setSize({"80%", 36});
-	titleLabel->setText("Project Settings");
-
-	layout->add(titleLabel);
+	bindTranslation(this->currentWindow,
+									   "dialog.project_settings._label",
+									   &tgui::ChildWindow::setTitle);
 
 	titleField = TextField::create();
-	titleField->label->setText("Title");
+	bindTranslation(titleField->label, "dialog.project_settings.title", &tgui::Label::setText);
 	titleField->setPosition({8, 48});
 	titleField->setSize({"80%", 24});
 	titleField->value->onTextChange([](const tgui::String &text) {
